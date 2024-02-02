53bbb5a7e1f5f6fa38bce5378a266d5d85c04223
TrevonWright <156366467+TrevonWright@users.noreply.github.com>
2024-02-02 20:13:33 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+# Furniture.py - This program calculates profits and sales prices for a furniture company.
+
+itemName = "TV Stand"
+retailPrice = 325.00
+wholesalePrice = 200.00
+
+# Write your assignment statements here for profit, salePrice, and saleProfit
+
+print("itemName: " + itemName)
+print("Retail Price: $" + str(325.00))
+print("Wholesale Price: $" + str(200.00))
+print("Profit: $" + str(125.00))
+print("Sale Price: $" + str(243.75))
+print("Sale Profit: $" + str(43.75))
